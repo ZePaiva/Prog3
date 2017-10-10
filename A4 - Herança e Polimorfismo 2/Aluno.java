c60985f7ecfda6c3e3b824cb791b61d268edaac0
José Paiva <zmppaiva99@gmail.com>
2017-10-11 00:03:07 +0100

    aula 4 adicionada, videoclube não dou garantias totais

---
+++ A4 - Herança e Polimorfismo 2/Aluno.java
@@ -0,0 +1,63 @@
+package aula4.e3;
+
+import aula1.e2.*;
+
+public class Aluno extends Socio
+{
+  private int nmec;
+  private String course;
+
+  public Aluno(Data insc, Data dof, String n, String cors, int c, int nm, int l)
+  {
+    super(insc, dof, n, c, l);
+    nmec = nm;
+    course = cors;
+  }
+
+  public String getCourse()
+  {
+    return course;
+  }
+
+  public int getNMec()
+  {
+    return nmec;
+  }
+  
+  @Override
+  public String toString()
+  {
+	  return " Sócio nº: " + super.getnSoc() + " -> Aluno nº: " + nmec + ", Nome: " + super.getNome() + " Curso: " + course;
+  }
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((course == null) ? 0 : course.hashCode());
+		result = prime * result + nmec;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (course == null)
+		{
+			if (other.course != null)
+				return false;
+		} else if (!course.equals(other.course))
+			return false;
+		if (nmec != other.nmec)
+			return false;
+		return true;
+	}
+}
---
+++ A4 - Herança e Polimorfismo 2/Socio.java
@@ -0,0 +1,109 @@
+package aula4.e3;
+
+import aula1.e2.*;
+
+public class Socio {
+	private static int seqSoc = 1;
+	private final int nSoc;
+	private final int nCC;
+	private final int maxLoans;
+	private final String nome;
+	private final Data dateOfBirth;
+	private final Data insc;
+	private int loans;
+	
+	public Socio(Data data, Data data2, String n, int cc, int loans)
+	{
+		nSoc = seqSoc++;
+		insc = data;
+		nome = n;
+		nCC = cc;
+		dateOfBirth = data2;
+		maxLoans = loans;
+		this.loans = 0;
+	}
+
+	public int getnSoc() 
+	{
+		return nSoc;
+	}
+
+	public int getnCC()
+	{
+		return nCC;
+	}
+
+	public String getNome() 
+	{
+		return nome;
+	}
+
+	public Data getDateOfBirth() 
+	{
+		return dateOfBirth;
+	}
+
+	public Data getInsc() 
+	{
+		return insc;
+	}	
+	
+	public boolean newLoan()
+	{
+		if (loans < maxLoans)
+		{
+			loans++;
+			return true;
+		}	
+		return false;
+	}
+	
+	public int loanAmount()
+	{
+		return loans;
+	}
+	
+	public void clearLoan()
+	{
+		loans--;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + nSoc;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Socio other = (Socio) obj;
+		if (nSoc != other.nSoc)
+			return false;
+		if (nome == null)
+		{
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Socio [nSoc=" + nSoc + ", nCC=" + nCC + ", nome=" + nome + ", loans=" + loans + "]";
+	}
+	
+	
+}
